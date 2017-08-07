487a4e1a7ddbe02f384da695270750d40360b8d3
masasutzu <suzuki_masayuki_0117@hotmail.co.jp>
2017-08-07 23:53:52 +0900

    hears()の第一引数に正規表現オプジェクトを渡すよう修正

--- chm_vocal.js
+++ chm_vocal.js
@@ -2,18 +2,6 @@ if (!process.env.token) {
     console.log('Error: Specify token in environment');
     process.exit(1);
 }
-
-var fs = require('fs');
-var vocal_map = new Map();
-
-fs.readdir('src/', function(err, files) {
-    if (err) throw err;
-
-    files.forEach(function (file) {
-        var json = JSON.parse(fs.readFileSync('src/' + file, 'utf8'));
-        vocal_map.set(json.word, json.reading);
-    });
-});
 var Botkit = require('./lib/Botkit.js');
 
 var controller = Botkit.slackbot({
@@ -22,6 +10,35 @@ var controller = Botkit.slackbot({
 var bot = controller.spawn({
     token: process.env.token
 }).startRTM();
+var vocal_map = new Map();
+
+try {
+    var fs = require('fs');
+    var files = fs.readdirSync('src/');
+
+    files.forEach(function (file) {
+        var json = JSON.parse(fs.readFileSync('src/' + file, 'utf8'));
+        vocal_map.set(json.word, json.reading);
+    });
+}
+catch (err) {
+    console.error(err);
+    process.exit(1);
+}
+var regexp_string = '('
+
+for (var key of vocal_map.keys()) {
+  regexp_string += key + '|'
+}
+regexp_string = regexp_string.replace(/\|$/, ')');
+var regexp = new RegExp(regexp_string, 'g');
+var target = 'direct_message,direct_mention,mention,ambient';
+
+controller.hears(regexp, target, function(bot, message) {
+    for (var matched of message.match) {
+        bot.reply(message, '```' + matched + ':' + vocal_map.get(matched) + '```');
+    }
+});
 
 controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function(bot, message) {
 
@@ -47,18 +64,5 @@ controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function
             }
         }
         ]);
-    });
-});
-var target = 'direct_message,direct_mention,mention,ambient';
-
-controller.hears(['(.*)'], target, function(bot, message) {
-    var text = message.text;
-
-    vocal_map.forEach (function(value, key, map) {
-        var reg = new RegExp(key);
-
-        if (text.match(reg)) {
-            bot.reply(message, '```' + key + ':' + value + '```');
-        }
     });
 });
