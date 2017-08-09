4a680caeff9733e001844a89e667995bfb621926
masasutzu <suzuki_masayuki_0117@hotmail.co.jp>
2017-08-10 05:00:57 +0900

    リファクタリング

--- chm_vocal.js
+++ chm_vocal.js
@@ -2,22 +2,20 @@ if (!process.env.token) {
     console.log('Error: Specify token in environment');
     process.exit(1);
 }
-var Botkit = require('./lib/Botkit.js');
-
-var controller = Botkit.slackbot({
+const Botkit = require('./lib/Botkit.js');
+const controller = Botkit.slackbot({
 });
-
-var bot = controller.spawn({
+const bot = controller.spawn({
     token: process.env.token
 }).startRTM();
-var vocal_map = new Map();
+const vocal_map = new Map();
 
 try {
-    var fs = require('fs');
-    var files = fs.readdirSync('src/');
+    const fs = require('fs');
+    const files = fs.readdirSync('src/');
 
     files.forEach(function (file) {
-        var json = JSON.parse(fs.readFileSync('src/' + file, 'utf8'));
+        const json = JSON.parse(fs.readFileSync('src/' + file, 'utf8'));
         vocal_map.set(json.word, json.reading);
     });
 }
@@ -25,25 +23,18 @@ catch (err) {
     console.error(err);
     process.exit(1);
 }
-var regexp_string = '('
-
-for (var key of vocal_map.keys()) {
-  regexp_string += key + '|'
-}
-regexp_string = regexp_string.replace(/\|$/, ')');
-var regexp = new RegExp(regexp_string, 'g');
-var target = 'direct_message,direct_mention,mention,ambient';
+const regexp_string = '(' + Array.from(vocal_map.keys()).join('|') + ')';
+const regexp = new RegExp(regexp_string, 'g');
+const target = 'direct_message,direct_mention,mention,ambient';
 
 controller.hears(regexp, target, function(bot, message) {
-    for (var matched of message.match) {
+    message.match.forEach(function(matched){
         bot.reply(message, '```' + matched + ':' + vocal_map.get(matched) + '```');
-    }
+    });
 });
 
 controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function(bot, message) {
-
     bot.startConversation(message, function(err, convo) {
-
         convo.ask('Are you sure you want me to shutdown?', [
             {
                 pattern: bot.utterances.yes,
